04906b8a0e37fc2461140e74451c5e9b74153e9b
Aditi Ailawadhi <aditi.ailawadhi@my.jcu.edu.au>
2021-06-15 16:15:24 +0530

    Add Programming Exercises.

---
+++ ch01/ProgrammingExercises/src/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Thought I'd end up with Sean\n" +
+                "But he wasn't a match\n" +
+                "Wrote some songs about Ricky\n" +
+                "Now I listen and laugh");
+    }
+}
---
+++ ch01/ProgrammingExercises/src/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Movie Quote: I feel the need—the need for speed!");
+        System.out.println("Movie: Top Gun");
+        System.out.println("Character: Pete Mitchell and Nick Bradshaw");
+        System.out.println("Movie Year: 1986");
+    }
+}
